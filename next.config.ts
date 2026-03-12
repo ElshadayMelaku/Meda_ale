49cfa01ffa64b19d6cab4979122f089616e3b9aa
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-03-12 13:05:51 +0300

    WIP backend rewrite

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // turbopack config removed as it's experimental and causing warnings
-};
-
-export default nextConfig;
